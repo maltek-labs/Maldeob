c92cbd569c51fcb9a1cfd55e3a3b4aa27519dc49
maltek-labs <77010288+maltek-labs@users.noreply.github.com>
2021-06-06 15:18:28 -0400

    Added new code changes+layout

---
+++ Maldeob-Static-REM-Framework/src/imports/JS.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import sys, argparse, re, jsbeautifier, base64, binascii, json, pathlib
+
+imports = pathlib.Path(r'Maldeob - Malware Deobfuscator/src/imports').resolve()
+outputs = pathlib.Path(r'Maldeob - Malware Deobfuscator/src/outputs').resolve()
+json_file = imports / r'Signatures.json'
+
+###############################################################################################################
+# Parses and pull data from Base64_array, decodes, then appends resulting files/data to File_list & Signature_Check arrays
+
+def Stage_Puller(Base64_Array, File_list, Signature_Check):
+    for item in Base64_Array:
+        if Base64_Array.count(item) >= 2:
+                while("" or '' in Base64_Array):
+                        Base64_Array.remove('')
+                        Base64_Array.remove("")  
+        
+                while Base64_Array.count(item) >= 2:
+                        Base64_Array.remove(item)
+        if len(item) >= 123:
+            File_list.append(base64.decodebytes(bytearray(item, 'ascii')))
+            Base64_Array.remove(item)
+            
+            while len(item) >= 17:
+                item = str(item)[:-1]
+            
+            Base64_Array.append(item)
+            data = str(binascii.hexlify(binascii.a2b_base64(item))).replace('b\'', '').replace('\'', '').upper()
+            Signature_Check.append(' '.join(data[i:i+2] for i in range(0,len(data),2)))
+
+    # Loads signatures from Signatures.json
+    with open(json_file, 'r') as data:
+        data = json.load(data)
+
+    # Checks signatures for matches based on magic byte value and outputs resulting file
+    for item in Signature_Check:
+        while not list(filter(lambda x:x["hex"] == item, data)):
+            if not list(filter(lambda x:x["hex"] == item, data)):
+                item = item[:-3]
+            else:
+                pass
+        
+        if list(filter(lambda x:x["hex"] == item, data)):
+            File_Found = list(filter(lambda x:x["hex"] == item, data))[0]['file_extension']
+            print('Signature(s) matched: \n\n'+ str(list(filter(lambda x:x["hex"] == item, data))[0]))
+            print('')
+        else:
+            return False
+            
+
+
+        item = 0
+        while item < len(File_list):
+            with open(f"{outputs}/Payload_{File_Found}.file", 'wb') as payload:
+                payload.write(File_list[item])
+
+            item += 1
+
+###############################################################################################################
+
+def Variable_Replacer(Converted_Text, Variable_Array):
+    i = 0
+    while i < len(Variable_Array):
+        
+        VarContents = re.search(r"=(.*?);", Variable_Array[i], re.MULTILINE)
+        
+        if VarContents:
+            
+            VarContents = str(VarContents.group(1)).strip()
+            VarName = re.search(r"var\s(.*?)[=\s]", Variable_Array[i], re.MULTILINE)
+            Array_Match = re.search(r"(?!\[\d\])(\[.*?\])", VarContents)
+            Function_Match = re.search(r"(\(.*\))", VarContents)
+            Index_Match = re.search(r"(\[\d\])", VarContents)
+            
+            if Array_Match:
+                pass
+            elif Function_Match:
+                pass
+            elif Index_Match:
+                pass
+            else:
+                Text_After = Converted_Text.replace(Variable_Array[i], '')
+                
+                if re.search(r'(\"\\\\\")', VarContents, re.MULTILINE):
+                    Converted_Text = re.sub(rf"\b{VarName.group(1)}\b", r'"\\\\"', Text_After)
+                else:
+                    Converted_Text = re.sub(rf"\b{VarName.group(1)}\b", ' ' + VarContents, Text_After)     
+        else:
+            pass
+        i += 1
+    return Converted_Text
+
+###########################################################################
+# Finds and replaces data in array if arrays exist
+def Array_Replacer(Converted_Text, Array_Name_Match1, Array_Name_Match2, Array_Name_Match3, Array_Name_Match4, Array_VarName, CompletedList, VarName_Array):
+    if re.search(r"(.*var.*\[.*?\])", Converted_Text, re.MULTILINE):
+    
+        # find and append Variable names found
+        if Array_Name_Match1:
+            VarName = Array_Name_Match1
+        elif Array_Name_Match2:
+            VarName = Array_Name_Match2 
+        elif Array_Name_Match3:
+            VarName = Array_Name_Match3
+        elif Array_Name_Match4:
+            VarName = Array_Name_Match4
+
+        if VarName in Array_VarName:
+            pass
+        else:
+            Array_VarName.append(VarName)
+
+        # Convert and replace Array contents found
+        c = 0
+        while c < len(Array_VarName[0]):
+            Array_Contents_Regex = rf"^.*{Array_VarName[0][c]}[=\s].*\[(.*?)\];"
+            Array_Contents_Name = re.search(Array_Contents_Regex, Converted_Text, re.MULTILINE)
+            
+            if Array_Contents_Name:
+                
+                ArrayContents = str(Array_Contents_Name.group(1)).replace("[", '').replace(']','')
+                ContentList = ArrayContents.strip().replace(' ', '').split(',')
+                
+                i = 0
+                b = 0
+                while i < len(ContentList):
+                    
+                    item = Array_VarName[0][c]+"[{}]".format(b).strip()
+                    text_after = Converted_Text.replace(item, ContentList[i])
+                    Converted_Text = text_after
+                    
+                    i += 1
+                    b = str(int(b)+1)
+                    
+                        
+                if Array_VarName[0][c] not in CompletedList:
+                    CompletedList.append(Array_VarName[0][c])
+
+                c += 1
+            else:
+                c += 1
+
+    i = 0
+    c = 0
+    while i < len(VarName_Array[0]):
+        regex = rf"^var\s{VarName_Array[i]}.*?\;$"
+        if Converted_Text.count(VarName_Array[i]) == 1:        
+            text_after = re.sub(regex, '', Converted_Text, flags=re.MULTILINE)
+            Converted_Text = text_after
+            
+        i += 1
+    return Converted_Text
+
+
+
---
+++ Maldeob-Static-REM-Framework/src/main.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+
+import sys, argparse, re, jsbeautifier, base64, binascii, json, pathlib
+from imports.JS import Variable_Replacer, Stage_Puller, Array_Replacer
+
+
+imports = pathlib.Path(r'Maldeob - Malware Deobfuscator/src/imports').resolve()
+outputs = pathlib.Path(r'Maldeob - Malware Deobfuscator/src/outputs').resolve()
+json_file = imports / r'Signatures.json'
+
+###############################################################################################################
+print( 
+                '''
+                #######################################################################################
+                #███╗   ███╗ █████╗ ██╗  ████████╗███████╗██╗  ██╗    ██╗      █████╗ ██████╗ ███████╗#
+                #████╗ ████║██╔══██╗██║  ╚══██╔══╝██╔════╝██║ ██╔╝    ██║     ██╔══██╗██╔══██╗██╔════╝#
+                #██╔████╔██║███████║██║     ██║   █████╗  █████╔╝     ██║     ███████║██████╔╝███████╗#
+                #██║╚██╔╝██║██╔══██║██║     ██║   ██╔══╝  ██╔═██╗     ██║     ██╔══██║██╔══██╗╚════██║#
+                #██║ ╚═╝ ██║██║  ██║███████╗██║   ███████╗██║  ██╗    ███████╗██║  ██║██████╔╝███████║#
+                #╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝    ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝#
+                #######################################################################################
+                #                           https://maltek-labs.com                                   #
+                #                       -Protection begins with analysis-                             #
+                #                                                                                     #
+                #                  Maltek Labs Static REM Framework(WIP) v0.6.5                       #
+                #######################################################################################
+                # optional arguments:                                                                 #
+                #   -h, --help            Show this help message and exit                             #
+                #   --version             Show program's version number and exit.                     #      
+                #                                                                                     #
+                # required arguments:                                                                 #
+                #   -i INPUT, --input INPUT                                                           #
+                #                            PATH to malicious script.                                #
+                #   -o OUTPUT, --output OUTPUT                                                        #
+                #                            PATH to output the completed file.                       #
+                #                                                                                     #
+                #######################################################################################
+                '''
+                )
+###############################################################################################################
+
+parser = argparse.ArgumentParser(description=' Maltek Labs Static REM Framework(WIP)', prog='Maldeob', add_help=False)
+
+#cmdline arguments to be passed
+parser.add_argument('-h','--help', action='store_true')
+parser.add_argument("-i", '--input', help="PATH to malicious script.", type=str)
+parser.add_argument("-o", "--output", help="PATH to output the completed file.", type=str)
+parser.add_argument('--version', action='version', version='%(prog)s 0.5')
+
+#sets up arguments
+args = vars(parser.parse_args())
+ifile = args['input']
+ofile = args['output']
+helpme = args['help']
+
+# Regex to pull input file extension
+if ifile is not False:
+    File_Extension = re.search(r'(?i)(\..*?$)', ifile, re.MULTILINE).group(1)
+
+###############################################################################################################
+
+if re.search('.js', File_Extension, re.IGNORECASE):
+
+#Js-Beautifier options
+    opts = jsbeautifier.BeautifierOptions
+    Converted_Text = str(jsbeautifier.beautify_file(ifile, opts))
+
+    opts.indent_size = 4
+    opts.indent_with_tabs = True
+    opts.editorconfig = False
+    opts.eol = '\n'
+    opts.end_with_newline = True
+    opts.indent_level = 0
+    opts.preserve_newlines = True
+    opts.max_preserve_newlines = 10
+    opts.space_in_paren = True
+    opts.space_in_empty_paren = False
+    opts.jslint_happy = False
+    opts.space_after_anon_function = True
+    opts.space_after_named_function = True
+    opts.brace_style = 'collapse'
+    opts.unindent_chained_methods = False
+    opts.break_chained_methods = False
+    opts.keep_array_indentation = True
+    opts.unescape_strings = False
+    opts.wrap_line_length = 0
+    opts.e4x = True
+    opts.comma_first = False
+    opts.indent_empty_lines = False
+    opts.templating = ['auto']
+    opts.space_before_conditional = True
+
+    #evals JS code. Future setting. 
+    opts.eval_code = False
+
+###############################################################################################################
+# Arrays
+    Array_VarName = []
+    CompletedList = []
+    Variable_Array = []
+    File_list = []
+    Signature_Check = []
+    VarName_Array = re.findall(r"^.*var\s(.*?)[;\s=]", Converted_Text, re.MULTILINE)
+    Base64_Array = re.findall(r"((?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/][AQgw]==|[A-Za-z0-9+\/]{2}[AEIMQUYcgkosw048]=)?)", Converted_Text, re.MULTILINE)
+    Full_Variable = re.findall(r"(^.*var\s.*?\;)", Converted_Text, re.MULTILINE)
+    Array_Name_Match1 = re.findall(r"var\s(.*?)\s\=\s\[", Converted_Text, re.MULTILINE)
+    Array_Name_Match2 = re.findall(r"var\s(.*?)\=\[", Converted_Text, re.MULTILINE)
+    Array_Name_Match3 = re.findall(r"var\s(.*?)\=\s\[", Converted_Text, re.MULTILINE)
+    Array_Name_Match4 = re.findall(r"var\s(.*?)\s\=\[", Converted_Text, re.MULTILINE)
+        
+###############################################################################################################
+# Clean up list data
+
+    for item in Full_Variable:
+        var = re.sub(R"\s{2,}", '', item)
+        Variable_Array.append(var)
+
+    for item in VarName_Array:
+        if VarName_Array.count(item) >= 2:
+            VarName_Array.remove(item)
+
+##############################################################################################################
+
+     
+if __name__ == "__main__":
+    
+    if helpme == True:
+        exit()
+        print('')
+
+    Stage_Puller(Base64_Array, File_list, Signature_Check)
+    Converted_Text = Variable_Replacer(Converted_Text, Variable_Array)
+    Converted_Text = Array_Replacer(Converted_Text, Array_Name_Match1, Array_Name_Match2, Array_Name_Match3, Array_Name_Match4, Array_VarName, CompletedList, VarName_Array)
+ 
+    with open(f'{outputs}/' + f'{ofile}', 'w') as w:
+        w.write(Converted_Text.strip().replace('\n',''))
+        print('##############################################################################################################')
+        print('')
+        print(Rf'The file has been outputted to: {outputs} as '+ ofile) 
+        print('')
+        print('Press enter to exit')
+        input()
+        exit()
